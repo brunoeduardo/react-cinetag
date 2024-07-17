d0654e64951c84b6de1deca672ef6e4f129e003c
Bruno Eduardo <bruno@beduardo.com>
2024-07-17 20:19:02 -0300

    footer component

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return(
+        <footer className={styles.footerContainer}>Developed by John Doe</footer>
+    )
+}
+
+export default Footer
